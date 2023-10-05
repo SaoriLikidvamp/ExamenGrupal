2fb69a093874e020016ea09600bb764d9e3e74dc
AdrianoVEPO <Adrianovenepo@gmail.com>
2023-10-05 18:20:57 -0500

    Creacion de la clase personaje(Adriano Venero)
    Programación de la clase de personaje

---
+++ ExamenGrupal/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupal
+{
+    class Personaje
+    {
+        public string Nombre;
+        public int Fuerza;
+        public int Destreza;
+        public int Vida;
+
+
+        public Personaje(string nombre, int fuerza, int destreza, int vida)
+        {
+            this.Nombre = nombre;
+            this.Fuerza = fuerza;
+           this.Destreza = destreza;
+            this.Vida = vida;
+        }
+    }
+}
