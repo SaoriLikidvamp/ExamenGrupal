21152dd5cb260eb56cc652b8dcca718e7f15ebd4
Roseling Marquez <41608842+SaoriLikidvamp@users.noreply.github.com>
2023-10-05 18:36:38 -0500

    herencia de Personaje, creación de clase Jugador  - Roseling
    creacion de clase jugador, la herencia de personaje y ahí se muestra al jugador creado.

---
+++ ExamenGrupal/Jugador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupal
+{
+    class Jugador : Personaje
+    {
+        public Jugador(string nombre, int fuerza, int destreza, int vida)
+        : base(nombre, fuerza, destreza, vida)
+        {
+        }
+
+        public void MostrarJugador()
+        {
+            Console.WriteLine($"----------------------------------");
+            Console.WriteLine($"--- Personaje Creado con Éxito ---");
+            Console.WriteLine($"----------------------------------");
+            Console.WriteLine($"Este es tu personaje creado:");
+            Console.WriteLine($" ");
+            Console.WriteLine($"Nombre: {Nombre}");
+            Console.WriteLine($"Fuerza: {Fuerza}");
+            Console.WriteLine($"Destreza: {Destreza}");
+            Console.WriteLine($"Vida: {Vida}");
+            Console.WriteLine($" ");
+        }
+    }
+}
