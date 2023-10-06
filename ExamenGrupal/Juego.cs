7305d3aba01a35c8529ea67492ca0de1c472065a
Roseling Marquez <41608842+SaoriLikidvamp@users.noreply.github.com>
2023-10-05 20:11:19 -0500

    Creación de personaje y validación Roseling
    Se crea el personaje y se valida que los stats no sobrepasen los 100, asi como que no meta letra o valores negativos
    
    Se genera la clase iniciar juego en el program

---
+++ ExamenGrupal/Juego.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenGrupal
+{
+    class Juego
+    {
+        private Jugador jugador;
+        public Juego(string nombre)
+        {
+            jugador = new Jugador(nombre, 0, 0, 0);
+        }
+
+        public void IniciarJuego()
+        {
+            Console.WriteLine("  ");
+            Console.WriteLine($"¡Bienvenido al juego de aventura de texto, {jugador.Nombre}!");
+            Console.WriteLine("  ");
+            Console.WriteLine("Tienes 100 puntos para distribuir entre tus atributos (fuerza, destreza y vida).");
+
+            while (true)
+            {
+                Console.WriteLine("  ");
+                Console.Write("Ingresa la cantidad de puntos para Fuerza: ");
+                int fuerza = Comprobando(0, 100 - jugador.Destreza - jugador.Vida);
+                Console.Write("Ingresa la cantidad de puntos para Destreza: ");
+                int destreza = Comprobando(0, 100 - jugador.Fuerza - jugador.Vida);
+                Console.Write("Ingresa la cantidad de puntos para Vida: ");
+                int vida = Comprobando(0, 100 - jugador.Fuerza - jugador.Destreza);
+                Console.WriteLine("  ");
+
+                if (fuerza + destreza + vida <= 100)
+                {
+                    jugador.Fuerza = fuerza;
+                    jugador.Destreza = destreza;
+                    jugador.Vida = vida;
+                    break; // Continuar con la historia
+                }
+                else
+                {
+                    Console.WriteLine("La suma de los puntos no debe ser mayor a 100. Inténtalo nuevamente.");
+                }
+            }
+
+            // Muestra el resumen del personaje creado
+            jugador.MostrarJugador();
+            Console.WriteLine("  ");
+            Console.Write("¡Ahora empezará la real aventura :D !");
+            Console.ReadLine();
+        }
+
+        private int Comprobando(int minimo, int maximo) // validacion de ingreso de numeros, que no ingrese ni letras ni numeros negativos
+        {
+            int valor;
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out valor) && valor >= minimo && valor <= maximo)
+                {
+                    return valor;
+                }
+                Console.Write($"Ingresa un número válido ({minimo}-{maximo}): ");
+            }
+        }
+    }
+}
--- ExamenGrupal/Program.cs
+++ ExamenGrupal/Program.cs
@@ -10,6 +10,13 @@ namespace ExamenGrupal
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-- Bienvenido a una aventura extraordinaria -- ");
+            Console.WriteLine("  ");
+            Console.Write("Ingresa el nombre de tu personaje: ");
+            string nombre = Console.ReadLine();
+
+            Juego juego = new Juego(nombre);
+            juego.IniciarJuego();
         }
     }
 }
